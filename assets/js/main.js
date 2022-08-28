34e1ae4b9171c1f9e315e327959e6c7ffc1f351c
yoma75 <fredyomayuza@gmail.com>
2022-08-28 16:59:40 -0500

    footer and others

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,6 +86,23 @@ var swiper = new Swiper(".product-slider", {
 
 // --------------------------------------------------------------
 
-
-
-
+// slider clients
+var swiper = new Swiper(".clients-slider", {
+  loop: true,
+  grabCursor: true,  
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
